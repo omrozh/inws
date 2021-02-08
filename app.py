87650e9746abbc736bded2cb469782c910d700ef
omrozh <omrozh@hotmail.com>
2021-02-08 18:08:58 +0300

    new commit

--- app.py
+++ app.py
@@ -9,69 +9,98 @@ app.config["SQLALCHEMY_DATABASE_URI"] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
 
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String)
+    password = db.Column(db.String, unique=True)
+
+
 class File(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     searchtitle = db.Column(db.String)
     filebytes = db.Column(db.LargeBinary, nullable=False)
     filename = db.Column(db.String, unique=True, nullable=False)
+    owner = db.Column(db.String)
 
     def __repr__(self):
         return self.filename
 
 
-@app.route("/<filename>", methods=["POST", "GET"])
-def mainPage(filename):
+@app.route("/<filename>/<password>/<username>", methods=["POST", "GET"])
+def mainPage(filename, password, username):
+    if not User.query.filter_by(username=username).first().password == password:
+        return None
     data = flask.request.get_data()
     try:
-        db.session.add(File(filename=filename, filebytes=data, searchtitle=filename.lower()))
+        db.session.add(File(filename=filename, filebytes=data, searchtitle=filename.lower(), owner=username))
         db.session.commit()
     except:
         return "Upload Error"
     return "File Uploaded Successfully"
 
 
-@app.route("/filereturn/<filename>/<password>")
-def returnFile(filename, password):
-    if not password == "omrozh-ings-infy":
+@app.route("/filereturn/<filename>/<password>/<username>")
+def returnFile(filename, password, username):
+    if not User.query.filter_by(username=username).first().password == password:
         return None
-    file = File.query.filter_by(filename=filename).first()
-    return file.filebytes
+
+    if File.query.filter_by(filename=filename).first().owner == username:
+        file = File.query.filter_by(filename=filename).first()
+        return file.filebytes
+    else:
+        return "None"
 
 
-@app.route("/search/<searchquery>/<password>")
-def returnSearch(searchquery, password):
-    if not password == "omrozh-ings-infy":
+@app.route("/search/<searchquery>/<password>/<username>")
+def returnSearch(searchquery, password, username):
+    if not User.query.filter_by(username=username).first().password == password:
         return None
     files = File.query.filter(File.searchtitle.like('%' + searchquery.lower() + '%'))
     filesarray = []
 
     for i in files:
-        filesarray.append(i.filename)
+        if i.owner == username:
+            filesarray.append(i.filename)
     return ", ".join(filesarray)
 
 
-@app.route("/list/<password>")
-def returnList(password):
-    if not password == "omrozh-ings-infy":
+@app.route("/list/<password>/<username>")
+def returnList(password, username):
+    if not User.query.filter_by(username=username).first().password == password:
         return None
 
     files = File.query.all()
     filesarray = []
 
     for i in files:
-        filesarray.append(i.filename)
+        if i.owner == username:
+            filesarray.append(i.filename)
     return ", ".join(filesarray)
 
 
-@app.route("/delete/<name>/<password>")
-def deleteFile(name, password):
-    if not password == "omrozh-ings-infy":
+@app.route("/delete/<name>/<password>/<username>")
+def deleteFile(name, password, username):
+    if not User.query.filter_by(username=username).first().password == password:
         return None
 
-    db.session.delete(File.query.filter_by(filename=name).first())
-    db.session.commit()
+    if File.query.filter_by(filename=name).first().owner == username:
+        db.session.delete(File.query.filter_by(filename=name).first())
+        db.session.commit()
 
-    return "File deleted"
+        return "File deleted"
+
+    else:
+        return "Unauthorized!"
+
+
+@app.route("/create/account/<password>/<username>")
+def createAccount(username, password):
+    try:
+        db.session.add(User(username=username, password=password))
+        db.session.commit()
+        return "Account successfully created!"
+    except:
+        return "Username already exists!"
 
 
 @app.route("/client/install")
--- push.py
+++ push.py
@@ -10,12 +10,16 @@ else:
     argument = None
 
 if option == "upload":
+    password = sys.argv[5]
+    username = sys.argv[4]
+
     load = open(argument, "rb")
     load_read = load.read()
 
     print("Reading file.")
 
-    req = request.Request(f"http://storagerequests.herokuapp.com/{input('Filename: ')}", data=load_read)
+    req = request.Request(f"http://storagerequests.herokuapp.com/"
+                          f"{input('Filename: ')}/{password}/{username}", data=load_read)
 
     print("POST Request is formed.")
     response = request.urlopen(req)
@@ -27,8 +31,9 @@ if option == "upload":
 
 elif option == "download":
     save_path = sys.argv[3]
-    password = sys.argv[4]
-    response = request.urlopen(f"http://storagerequests.herokuapp.com/filereturn/{argument}/{password}")
+    password = sys.argv[5]
+    username = sys.argv[4]
+    response = request.urlopen(f"http://storagerequests.herokuapp.com/filereturn/{argument}/{password}/{username}")
     read_response = response.read()
 
     save = open(save_path, "wb")
@@ -38,19 +43,28 @@ elif option == "download":
 
 
 elif option == "search":
-    password = sys.argv[3]
-    response = request.urlopen(f"http://storagerequests.herokuapp.com/search/{argument}/{password}")
+    password = sys.argv[4]
+    username = sys.argv[3]
+    response = request.urlopen(f"http://storagerequests.herokuapp.com/search/{argument}/{password}/{username}")
     read_response = response.read()
     print(read_response.decode("utf-8"))
 
 elif option == "list":
-    password = sys.argv[2]
-    response = request.urlopen(f"http://storagerequests.herokuapp.com/list/{password}")
+    password = sys.argv[3]
+    username = sys.argv[2]
+    response = request.urlopen(f"http://storagerequests.herokuapp.com/list/{password}/{username}")
     read_response = response.read()
     print(read_response.decode("utf-8"))
 
 elif option == "delete":
-    password = sys.argv[3]
-    response = request.urlopen(f"http://storagerequests.herokuapp.com/delete/{argument}/{password}")
+    password = sys.argv[4]
+    username = sys.argv[3]
+    response = request.urlopen(f"http://storagerequests.herokuapp.com/delete/{argument}/{password}/{username}")
     read_response = response.read()
     print(read_response.decode("utf-8"))
+
+elif option == "createAccount":
+    username = argument
+    password = sys.argv[3]
+
+    print(request.urlopen(f"http://storagerequests.herokuapp.com/create/account/{password}/{username}").read())
